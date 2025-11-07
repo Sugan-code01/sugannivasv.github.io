f33d49d5948aba4b72baaaaa1d030065239801fc
sugannivas.v <sugannivas.v@gmail.com>
2025-11-07 11:27:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const text = "Python & Django Developer";
+let i = 0;
+function typeWriter() {
+  if (i < text.length) {
+    document.getElementById("typewriter").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeWriter, 100);
+  }
+}
+window.onload = typeWriter;
